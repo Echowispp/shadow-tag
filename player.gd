230a28442bd71a47378a72f77bd98611fd7ae992
Echowispp <kauri.sykari190@gmail.com>
2025-11-01 14:11:27 +0200

    Added flashlight timer and shadow unstucking
    Also comes with a jumpscare mechanic :D

--- player.gd
+++ player.gd
@@ -1,12 +1,14 @@
-extends CharacterBody2D
+extends CharacterBody2D;
 
 
-@export var speed = 35.0
+@export var speed = 50.0;
+@export var flashlight_timer_max = 10;
+var flashlight_timer = flashlight_timer_max;
 
-@onready var animated_sprite = $AnimatedSprite2D
+@onready var animated_sprite = $AnimatedSprite2D;
+@onready var flashlight = $PlayerFlashlight;
+@onready var flashlight_trigger = $PlayerFlashlight/Area2D/CollisionShape2D
 
-#func _ready():
-	#pass
 ## CHECK GDOCS TODO!!!!
 ## CHECK GDOCS TODO!!!!
 ## CHECK GDOCS TODO!!!!
@@ -17,20 +19,20 @@ extends CharacterBody2D
 ## CHECK GDOCS TODO!!!!
 #when reviewing code, ignore the above. I'm trying to use a todo list between sessions to remember what I need to do next, I'm afraid I'll forget it otherwise. 
 func _physics_process(delta: float) -> void:
-	velocity = Vector2.ZERO
+	velocity = Vector2.ZERO;
 	if (Input.is_action_pressed("move_right")):
-		velocity.x += 1
+		velocity.x += 1;
 	if (Input.is_action_pressed("move_left")):
-		velocity.x -= 1
+		velocity.x -= 1;
 	if (Input.is_action_pressed("move_up")):
-		velocity.y -= 1
+		velocity.y -= 1;
 	if (Input.is_action_pressed("move_down")):
-		velocity.y += 1
+		velocity.y += 1;
 	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		move_and_slide()
-		var movement_angle_rad = velocity.angle()
-		var movement_angle = movement_angle_rad * 180 / PI
+		velocity = velocity.normalized() * speed;
+		move_and_slide();
+		var movement_angle_rad = velocity.angle();
+		var movement_angle = movement_angle_rad * 180 / PI;
 		if movement_angle > -45 and movement_angle < 45:
 				animated_sprite.play("walk_h");
 				animated_sprite.flip_h = false;
@@ -44,4 +46,16 @@ func _physics_process(delta: float) -> void:
 			animated_sprite.play("walk_up");
 			animated_sprite.flip_h = false;
 	else:
-		animated_sprite.play("idle")
+		animated_sprite.play("idle");
+	if flashlight_timer > 0 and Input.is_action_pressed("flashlight_on"):
+		var mouse_pos = get_global_mouse_position();
+		flashlight.look_at(mouse_pos);
+		flashlight.rotation_degrees += 90;  # I didn't want to just move the flashlight orientation :/
+		flashlight.enabled = true;
+		flashlight_trigger.disabled = false;
+		flashlight_timer -= delta;
+	else:
+		flashlight.enabled = false;
+		flashlight_trigger.disabled = true;
+		if flashlight_timer < flashlight_timer_max and not Input.is_action_pressed("flashlight_on"):
+			flashlight_timer += (delta * 0.5);
--- shadow.gd
+++ shadow.gd
@@ -1,20 +1,23 @@
-extends CharacterBody2D
+extends CharacterBody2D;
 
-@export var speed = 30.0
+@export var speed = 200.0;
 
-@onready var player = get_tree().get_first_node_in_group("player")
-@onready var animated_sprite = $AnimatedSprite2D
-@onready var shadow_area = $Area2D
+@onready var player = get_tree().get_first_node_in_group("player");
+@onready var animated_sprite = $AnimatedSprite2D;
+@onready var shadow_area = $Area2D;
+@onready var collision
 
-var is_in_light = false
+var is_in_light = false;
+var light_timer = 0.0;
+var invis_timer = 0.0;
 
 func _ready():
-	print("Shadow area: ", shadow_area)
-	shadow_area.area_entered.connect(_on_area_entered)
-	shadow_area.area_exited.connect(_on_area_exited)
+	shadow_area.area_entered.connect(_on_area_entered);
+	shadow_area.area_exited.connect(_on_area_exited);
 
-func _physics_process(_delta: float):
+func _physics_process(delta: float):
 	if not is_in_light:
+		light_timer -= delta;
 		var direction = (player.global_position - global_position).normalized();
 		velocity = direction * speed;
 		move_and_slide();
@@ -33,11 +36,24 @@ func _physics_process(_delta: float):
 			elif movement_angle >= -135 and movement_angle <= -45:
 				animated_sprite.play("walk_up");
 				animated_sprite.flip_h = false;
+	elif light_timer < 5: 
+		animated_sprite.play("idle");
+		light_timer += delta;
+	elif light_timer > 5:
+		light_timer = 0.0;
+		global_position -= (player.global_position - global_position); # unstuck by going to the other side of the player
+		visible = false; # make the shadow invisible to let it 
+		invis_timer = 0.0 + delta;
+	if invis_timer > 0 and invis_timer < 5:
+		invis_timer += delta
+	if invis_timer > 5 or (player.global_position - global_position).length() < 150:
+		visible = true
+	print ("Visible: ", visible, " invis_timer: ", invis_timer, " light_timer: ", light_timer)
 
 func _on_area_entered(area):
 	if area.get_parent() != null and area.get_parent().name == "PlayerFlashlight":
-		is_in_light = true
+		is_in_light = true;
 
 func _on_area_exited(area):
 	if area.get_parent() != null and area.get_parent().name == "PlayerFlashlight":
-		is_in_light = false
+		is_in_light = false;
